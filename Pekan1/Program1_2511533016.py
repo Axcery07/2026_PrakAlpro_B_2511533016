92e82d933c81b114ad125cd6405f04c23c4b5301
Axcery07 <axcery07@gmail.com>
2026-09-03 10:21:09 +0700

    Initial commit

---
+++ Pekan1/Program1_2511533016.py
@@ -0,0 +1,12 @@
+# Program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("Hello World")
+print()
+print("Program ini menghasilkan")
+print("Empat Baris output")
+print("/ \\ \\\ /// \\\\\\")
+print("Program ini" + " dibuat oleh a/n" + " Muhammad Fedora Argadyaksa dari 2511533016")
