c4ed7fc4da788faca728707c01239d477110286c
Axcery07 <axcery07@gmail.com>
2026-09-10 10:21:52 +0700

    LapPrak_ALpro1

---
+++ Pekan1/tugas1_2511533016.py
@@ -0,0 +1,16 @@
+"""
+Program Bebas - Perkenalan Diri
+Hanya menggunakan fungsi print()
+"""
+
+print("=================================")
+print("      PERKENALAN DIRI SAYA      ")
+print("=================================")
+print("Nama    : Muhammad Fedora Argadyaksa")
+print("NIM     : 2511533016")
+print("Jurusan : Informatika")
+print("Hobi    : Ngoding dan belajar hal baru")
+print("=================================")
+print("Ini adalah program Python sederhana yang dibuat untuk keperluan praktikum.")
+print("Terima kasih sudah menjalankan program ini!")
+print("=================================")
